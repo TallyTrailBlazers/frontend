0a3cef9e9d24ef41ac09e31bb4c67a0ce3e16ced
Casey McLaughlin <caseyamcl@gmail.com>
2016-04-24 11:27:33 -0400

    Fixed front page styles for smaller phones.

--- app/app.js
+++ app/app.js
@@ -15,7 +15,7 @@ config(['$routeProvider', '$httpProvider', '$locationProvider', 'authProvider', 
         templateUrl: 'trailpage/trailpage.html',
         controller: 'TrailPageCtrl'
       })
-  .otherwise({redirectTo: '/trail/9'});
+  .otherwise({redirectTo: '/trail/140'});
 
   authProvider.init({
     domain:   'caseyamcl.auth0.com',
