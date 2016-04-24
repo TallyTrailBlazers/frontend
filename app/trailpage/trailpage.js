e502a0043c2e55d5d538044a71e18bce2c28edd2
Casey McLaughlin <caseyamcl@gmail.com>
2016-04-24 02:13:23 -0400

    Working on middle screen

--- app/trailpage/trailpage.js
+++ app/trailpage/trailpage.js
@@ -58,4 +58,11 @@ angular.module('myApp.trailpage', ['ngRoute', 'angular-storage', 'angular-input-
     }
   };
 
-}]);
+}])
+
+.filter('capitalize', function() {
+  return function(input, all) {
+    var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
+    return (!!input) ? input.replace(reg, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
+  }
+});
